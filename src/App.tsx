db399a392222245bd3bccfac0485b765ad91b6be
maksim shirant <shirant.maksim@yandex.ru>
2025-08-14 15:56:23 +0300

    Query req

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 
 import './App.css'
-import {useState} from "react";
+import {useQuery} from "@tanstack/react-query";
+import axios from 'axios';
+
+
+interface PostProps {
+    userId?: string,
+    id: number,
+    title: string,
+    body: string,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
+    const {data, isLoading} = useQuery({
+        queryKey: ['posts'],
+        queryFn: async () => {
+          const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            return response.data
+        }
+    })
   return (
 
    <div>
-     <div>{count}</div>
-       <div>
-           <button disabled={count === 0 ? true : false} onClick={() => setCount(count - 1)}>-</button>
-           <button onClick={() => setCount(count + 1)}>+</button>
-       </div>
+       {!isLoading ? (data.map((post: PostProps) => (
+           <div>
+               <div key={post.id}>{post.id}</div>
+               <div>
+                   <div>{post.title}</div>
+                   <div>{post.body}</div>
+               </div>
 
+           </div>
+
+       ))) : (<div>Нет постов...</div>)
+       }
    </div>
   )
 }
