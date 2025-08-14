f44c8af5d28a97b3e4949825c0dd0bf7b145e1a3
maksim shirant <shirant.maksim@yandex.ru>
2025-08-14 13:55:18 +0300

    Count done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import {useState} from "react";
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-  Служит
-    </>
+
+   <div>
+     <div>{count}</div>
+     <button onClick={() => setCount(count + 1)}>+</button>
+   </div>
   )
 }
 
